57a44ab70abece6e9f9d967bcf985a9b55f00ee4
Sandy Carter <sandy.carter@savoirfairelinux.com>
2013-11-13 12:53:49 -0500

    [ADD] Travel Journey Module

---
+++ travel_journey/travel_journey.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, orm
+
+
+class travel_journey(orm.Model):
+    _description = 'Journey of travel'
+    _name = 'travel.journey'
+    _columns = {
+        'from': fields.many2one('res.country.state.city', 'City', required='True',
+                                help='Source city of travel.'),
+        'to': fields.many2one('res.country.state.city', 'City', required='True',
+                              help='Destination city of travel.'),
+    }
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
