897992372acb804fe5d35685d49057db9ba475fa
Sandy Carter <sandy.carter@savoirfairelinux.com>
2014-09-24 19:02:18 -0400

    Fix travis and line lengths
    Signed-off-by: Sandy Carter <sandy.carter@savoirfairelinux.com>

--- passport_expiration/__init__.py
+++ passport_expiration/__init__.py
@@ -20,6 +20,6 @@
 #
 ###############################################################################
 
-import hr
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+from . import (
+    hr,
+)
