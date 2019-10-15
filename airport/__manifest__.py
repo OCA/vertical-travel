5be47926fbac2b7e492992330d3a75cca16d9cf3
Tima <tima@invitu.com>
2019-10-15 09:15:11 -1000

    [MIG] airport: Migration to 11.0

---
+++ airport/__manifest__.py
@@ -0,0 +1,51 @@
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Partner Airport',
+    'version': '11.0.1.0.0',
+    'category': 'Customer Relationship Management',
+    'description': """
+Partner Airport
+===============
+This module allows to add :
+* airport : boolean
+* iata_code : char, size=3
+
+Contributors
+------------
+* Sandy Carter (sandy.carter@savoirfairelinux.com)
+* El Hadji Dem (elhadji.dem@savoirfairelinux.com)
+    """,
+    'author': "Savoir-faire Linux,Odoo Community Association (OCA)",
+    'website': 'http://www.savoirfairelinux.com',
+    'license': 'AGPL-3',
+    'depends': ['transportation', ],
+    'external_dependencies': {},
+    'data': [
+        'views/res_partner_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'images': [],
+}
---
+++ airport/models/res_partner.py
@@ -0,0 +1,33 @@
+###############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+
+    """Inherits partner and adds airport and iata_code fields
+    in the partner form
+    """
+    _inherit = 'res.partner'
+
+    airport = fields.Boolean(string='Airport', default=0)
+    iata_code = fields.Char(string='IATA Airline code', size=3)
