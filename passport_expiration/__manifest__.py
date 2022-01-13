e19619cf8104579fb474397a1fa5720ad4aef016
Damien Crier <damien.crier@camptocamp.com>
2022-01-13 10:53:48 +0100

    [MIG] passport_expiration: Migration to 15.0

--- passport_expiration/__manifest__.py
+++ passport_expiration/__manifest__.py
@@ -1,9 +1,10 @@
 # Copyright 2013 Savoir-faire Linux
 # Copyright 2021 Camptocamp SA
+# Copyright 2022 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Passport Expiration",
-    "version": "14.0.0.1.0",
+    "version": "15.0.0.1.0",
     "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/vertical-travel",
     "license": "AGPL-3",
